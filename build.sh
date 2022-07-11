44ed29861c45272517e3788ce97d451633d29e91
cptnalf <captainalf@gmail.com>
2022-07-11 17:45:45 -0400

    use buildkit.

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 export FFMPEG_VERSION=4.3.2
-#export DOCKER_BUILDKIT=1
+export DOCKER_BUILDKIT=1
 
 # this needs to say 'nvidia' for this to work.
-docker info | grep "Default Runtime:"
+#docker info | grep "Default Runtime:"
 
 docker build . \
-  -t cptnalf/jetson-ffmpeg-frigate:$FFMPEG_VERSION \
+ --progress=plain \
+  -f Dockerfile \
+  -t cptnalf/ffmpeg-l4t:${FFMPEG_VERSION}-r32.7.1 \
   --build-arg FFMPEG_VERSION=$FFMPEG_VERSION
